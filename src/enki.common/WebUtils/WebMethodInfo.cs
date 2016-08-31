279a95e85f8b52b3db5ef0743b0d2a4993570b98
reinaldo.coelho <reinaldo.coelho@gmail.com>
2016-08-31 09:42:44 -0300

    Inclusão de mais algumas classes compativeis.

---
+++ src/enki.common/WebUtils/WebMethodInfo.cs
@@ -0,0 +1,46 @@
+﻿namespace Enki.Common
+{
+    /// <summary>
+    /// Information about a web service operation
+    /// </summary>
+    public class WebMethodInfo
+    {
+        private string _name;
+        private Parameter[] _inputParameters;
+        private Parameter[] _outputParameters;
+
+        /// <summary>
+        /// OperationInfo
+        /// </summary>
+        public WebMethodInfo(string name, Parameter[] inputParameters, Parameter[] outputParameters)
+        {
+            _name = name;
+            _inputParameters = inputParameters;
+            _outputParameters = outputParameters;
+        }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        /// <summary>
+        /// InputParameters
+        /// </summary>
+        public Parameter[] InputParameters
+        {
+            get { return _inputParameters; }
+        }
+
+        /// <summary>
+        /// OutputParameters
+        /// </summary>
+        public Parameter[] OutputParameters
+        {
+            get { return _outputParameters; }
+        }
+    }
+}
---
+++ src/enki.common/WebUtils/WebMethodInfoCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Enki.Common
+{
+    /// <summary>
+    /// A collection of WebMethodInfo objects
+    /// </summary>
+    public class WebMethodInfoCollection : KeyedCollection<string, WebMethodInfo>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public WebMethodInfoCollection() : base() { }
+
+        protected override string GetKeyForItem(WebMethodInfo webMethodInfo)
+        {
+            return webMethodInfo.Name;
+        }
+    }
+}
