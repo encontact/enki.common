9377a3f92bd8e486c0e54180be5e869fd7c7b639
reinaldo <reinaldo.coelho@gmail.com>
2018-08-21 20:41:47 -0300

    Ajuste da biblioteca comum de funções Enki para incluir processamento de conversão HTML para Texto.

---
+++ src/enki.common.core/WebUtils/HtmlToText.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Enki.Common.WebUtils
+{
+    public class HtmlToText
+    {
+        public string OriginalHtml { get; private set; }
+
+        public HtmlToText(string html)
+        {
+            OriginalHtml = html;
+        }
+
+        /// <summary>
+        /// Recupera o texto do HTML
+        /// </summary>
+        /// <returns>Texto puro em UTF-8 do Html.</returns>
+        public string GetText()
+        {
+            return GetText(Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Recupera o texto do Html
+        /// </summary>
+        /// <param name="encode">Encode de retorno desejado.</param>
+        /// <returns></returns>
+        public string GetText(Encoding encode)
+        {
+            var lineBreakReplaced = Regex.Replace(OriginalHtml, "<br>|<br/>", "\r\n", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var paragraphReplaced = Regex.Replace(lineBreakReplaced, "<p>|</p>", "\r\n", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var divReplaced = Regex.Replace(paragraphReplaced, "<div>|</div>", "\r\n", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var removedStyleAndScript = Regex.Replace(paragraphReplaced, "(<style.+?</style>)|(<script.+?</script>)", "", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            var removedTags = Regex.Replace(removedStyleAndScript, "<[^>]+>", " ", RegexOptions.IgnoreCase);
+            var decodedHtml = HttpUtility.HtmlDecode(removedTags);
+            var final = EncodeString(decodedHtml, encode);
+            return final.Trim();
+        }
+
+        /// <summary>
+        /// Converte o texto informado para o encode Desejado.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string EncodeString(string text, Encoding encode)
+        {
+            byte[] utf = encode.GetBytes(text);
+            string strEncoded = encode.GetString(utf);
+            return strEncoded;
+        }
+    }
+}
---
+++ test/enki.common.core.test/WebUtils/HtmlToTextTests.cs
@@ -0,0 +1,146 @@
+﻿using System.Text;
+using Xunit;
+
+namespace Enki.Common.WebUtils.Tests
+{
+    public class HtmlToTextTests
+    {
+        [Fact]
+        public void MustProcessHtmlWithComment()
+        {
+            var html = @"<html xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:o=""urn:schemas-microsoft-com:office:office"" xmlns:w=""urn:schemas-microsoft-com:office:word"" xmlns:m=""http://schemas.microsoft.com/office/2004/12/omml"" xmlns=""http://www.w3.org/TR/REC-html40""><head><meta http-equiv=Content-Type content=""text/html; charset=iso-8859-1""><meta name=Generator content=""Microsoft Word 15 (filtered medium)""><!--[if !mso]><style>v\:* {behavior:url(#default#VML);}
+o\:* {behavior:url(#default#VML);}
+w\:* {behavior:url(#default#VML);}
+.shape {behavior:url(#default#VML);}
+</style><![endif]--><style><!--
+/* Font Definitions */
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+/* Style Definitions */
+p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin:0cm;
+	margin-bottom:.0001pt;
+	font-size:11.0pt;
+	font-family:""Calibri"",sans-serif;
+	mso-fareast-language:EN-US;}
+a:link, span.MsoHyperlink
+	{mso-style-priority:99;
+	color:#0563C1;
+	text-decoration:underline;}
+a:visited, span.MsoHyperlinkFollowed
+	{mso-style-priority:99;
+	color:#954F72;
+	text-decoration:underline;}
+span.EstiloDeEmail17
+	{mso-style-type:personal-compose;
+	font-family:""Calibri"",sans-serif;
+	color:windowtext;}
+.MsoChpDefault
+	{mso-style-type:export-only;
+	font-family:""Calibri"",sans-serif;
+	mso-fareast-language:EN-US;}
+@page WordSection1
+	{size:612.0pt 792.0pt;
+	margin:70.85pt 3.0cm 70.85pt 3.0cm;}
+div.WordSection1
+	{page:WordSection1;}
+--></style><!--[if gte mso 9]><xml>
+<o:shapedefaults v:ext=""edit"" spidmax=""1026"" />
+</xml><![endif]--><!--[if gte mso 9]><xml>
+<o:shapelayout v:ext=""edit"">
+<o:idmap v:ext=""edit"" data=""1"" />
+</o:shapelayout></xml><![endif]--></head><body lang=PT-BR link=""#0563C1"" vlink=""#954F72""><div class=WordSection1><p class=MsoNormal>Vamos colocar uma imagem supimpa???<o:p></o:p></p><p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><span style='mso-fareast-language:PT-BR'><img width=248 height=628 id=""Imagem_x0020_1"" src=""cid:image001.png@01D1C727.AF34A550""></span><o:p></o:p></p></div></body></html>";
+            var converter = new HtmlToText(html);
+            Assert.Equal("Vamos colocar uma imagem supimpa???", converter.GetText());
+        }
+        [Fact]
+        public void MustProcessHtmlWithStyleTag()
+        {
+            var html = @"
+            <html xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:o=""urn:schemas-microsoft-com:office:office"" xmlns:w=""urn:schemas-microsoft-com:office:word"" xmlns:m=""http://schemas.microsoft.com/office/2004/12/omml"" xmlns=""http://www.w3.org/TR/REC-html40"">
+                <head>
+                    <style>
+                        * {behavior:url(#default#VML);}
+                    </style>
+                </head>
+                <body lang=PT-BR link=""#0563C1"" vlink=""#954F72"">
+                    <div class=WordSection1>
+                        <p class=MsoNormal>Vamos colocar uma imagem supimpa???<o:p></o:p></p>
+                        <p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><span style='mso-fareast-language:PT-BR'>
+                        <img width=248 height=628 id=""Imagem_x0020_1"" src=""cid:image001.png@01D1C727.AF34A550""></span><o:p></o:p></p>
+                    </div>
+                </body>
+            </html>";
+            var converter = new HtmlToText(html);
+            Assert.Equal("Vamos colocar uma imagem supimpa???", converter.GetText());
+        }
+        [Fact]
+        public void MustProcessHtmlWithScriptTag()
+        {
+            var html = @"
+            <html xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:o=""urn:schemas-microsoft-com:office:office"" xmlns:w=""urn:schemas-microsoft-com:office:word"" xmlns:m=""http://schemas.microsoft.com/office/2004/12/omml"" xmlns=""http://www.w3.org/TR/REC-html40"">
+                <head>
+                    <style>
+                        * {behavior:url(#default#VML);}
+                    </style>
+                </head>
+                <body lang=PT-BR link=""#0563C1"" vlink=""#954F72"">
+                    <div class=WordSection1>
+                        <p class=MsoNormal>Vamos colocar uma imagem supimpa???<o:p></o:p></p>
+                        <p class=MsoNormal><o:p>&nbsp;</o:p></p><p class=MsoNormal><span style='mso-fareast-language:PT-BR'>
+                        <img width=248 height=628 id=""Imagem_x0020_1"" src=""cid:image001.png@01D1C727.AF34A550""></span><o:p></o:p></p>
+                    </div>
+                </body>
+                <script>
+                    function test(){
+                        alert('This is a Test');
+                    }
+                </script>
+            </html>";
+            var converter = new HtmlToText(html);
+            Assert.Equal("Vamos colocar uma imagem supimpa???", converter.GetText());
+        }
+        [Fact]
+        public void MustProcessHtmlWithUnicodeChars()
+        {
+            // TODO: A string criada abaixo não está representando um texto unicode real. Verificar para validar.
+            var html = Encoding.Unicode.GetString(Encoding.Unicode.GetBytes(@"
+            <html xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:o=""urn:schemas-microsoft-com:office:office"" xmlns:w=""urn:schemas-microsoft-com:office:word"" xmlns:m=""http://schemas.microsoft.com/office/2004/12/omml"" xmlns=""http://www.w3.org/TR/REC-html40"">
+                <head>
+                    <style>
+                        * {behavior:url(#default#VML);}
+                    </style>
+                </head>
+                <body lang=PT-BR link=""#0563C1"" vlink=""#954F72"">
+                    <p class=3DMsoNormal>Esta mensagem foi verificada pelo sistema de antivírus e  acredita-se estar livre de perigo.</p>
+                </body>
+                <script>
+                    function test(){
+                        alert('This is a Test');
+                    }
+                </script>
+            </html>"));
+            var converter = new HtmlToText(html);
+            var result = converter.GetText();
+            Assert.Equal("Esta mensagem foi verificada pelo sistema de antivírus e  acredita-se estar livre de perigo.", result);
+        }
+        [Fact]
+        public void MustProcessHtmlWithMultilineString()
+        {
+            var html = @"<html><head>
+<meta http-equiv=""content-type"" content=""text/html; charset=utf-8""></head>
+<body dir=""auto"">
+<div>Boa tarde,</div><div><br></div>
+<div>Gostaria de saber valores em caso de alteracao do voo de volta para quinta feira final do dia.</div>
+<div>Voo direto para sdu ou galeao.</div>
+<div>Favor sinalizar voo a partir das 18h com menor valor</div>
+<div><br></div>
+<div>Obrigada<br><br><div>Sent from my iPhone</div></div><div><br>";
+            var converter = new HtmlToText(html);
+            var result = converter.GetText();
+            Assert.Contains("\r\n", result);
+        }
+    }
+}
+
